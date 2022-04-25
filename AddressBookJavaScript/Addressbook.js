aa34f75d8a72ea0d7f2b9b61d0c5acd773a7e079
Resh@308 <reshsingh1995@gmail.com>
2022-04-25 11:55:15 +0530

    Added:Program for add new address book

--- AddressBookJavaScript/Addressbook.js
+++ AddressBookJavaScript/Addressbook.js
@@ -1,4 +1,11 @@
-class Contact{
+const prompt = require('prompt-sync')();
+const NAME_REGEX_PATTERN = RegExp('^[A-Z]{1}[a-z]{2,}$');
+const ADDRESS_REGEX_PATTERN = RegExp('^[a-zA-z]{3,}$');
+const PINCODE_REGEX_PATTERN = RegExp('^[1-9]{1}[0-9]{2}\\s{0,1}[0-9]{3}$');
+const PHONE_NUMBER_PATTERN = RegExp('^(0/91)?[6-9][0-9]{9}$');
+const EMAIL_REGEX_PATTERN = RegExp('^[a-zA-Z0-9]+([._+-][0-9a-zA-Z]+)*@[a-zA-Z0-9]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2,4})?$');
+
+class Contact {
     //property
     firstName;
     lastName;
@@ -10,25 +17,84 @@ class Contact{
     email;
 
     //constructor
-    constructor(firstName,lastName,address,city,state,zip,phoneNumber,email){
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.address = address;
-        this.city = city;
-        this.state = state;
-        this.zip = zip;
-        this.phoneNumber = phoneNumber;
-        this.email = email
+    constructor(firstName, lastName, address, city, state, zip, phoneNumber, email) {
+        if (!NAME_REGEX_PATTERN.test(firstName)) {
+            console.log('Please enter valid firstname.');
+        }
+        else {
+            this.firstName = firstName;
+        }
+        if (!NAME_REGEX_PATTERN.test(lastName)) {
+            console.log('Please enter valid lastname.');
+        }
+        else {
+            this.lastName = lastName;
+        }
+        if (!ADDRESS_REGEX_PATTERN.test(address)) {
+            console.log('Please enter valid address.');
+        }
+        else {
+            this.address = address;
+        }
+        if (!ADDRESS_REGEX_PATTERN.test(city)) {
+            console.log('Please enter valid city.');
+        }
+        else {
+            this.city = city;
+        }
+        if (!ADDRESS_REGEX_PATTERN.test(state)) {
+            console.log('Please enter valid state.');
+        }
+        else {
+            this.state = state;
+        }
+        if (!PINCODE_REGEX_PATTERN.test(zip)) {
+            console.log('Please enter valid pincode.');
+        }
+        else {
+            this.zip = zip;
+        }
+        if (!PHONE_NUMBER_PATTERN.test(phoneNumber)) {
+            console.log('Please enter valid phone number.');
+        }
+        else {
+            this.phoneNumber = phoneNumber;
+        }
+        if (!EMAIL_REGEX_PATTERN.test(email)) {
+            console.log('Please enter valid email ID.');
+        }
+        else {
+            this.email = email;
+        }
     }
 
     //method
-    toString(){
-        return "Firstname: "+this.firstName+" Lastname: "+this.lastName+" Address: "+this.address+" City: "+this.city+
-        " State: "+this.state+" Zip: "+this.zip+" Phone Number: "+this.phoneNumber+" Email: "+this.email;
+    toString() {
+        return "Firstname: " + this.firstName + " Lastname: " + this.lastName + " Address: " + this.address + " City: " + this.city +
+            " State: " + this.state + " Zip: " + this.zip + " Phone Number: " + this.phoneNumber + " Email: " + this.email;
     }
 }
 
-let person1 = new Contact("Resh","Singh","Abc","Bhopal","MH",462042,9009009010,"ndbbsmnb@gmail.com");
-console.log(person1.toString()); 
-let person2 = new Contact("Naresh","Singh","Abc","Sagar","MH",462024,9009009043,"nbsccmnsc@gmail.com");
-console.log(person2.toString()); 
+//array to store contacts
+let addressBookArray = new Array();
+let countEntry = 0;
+do {
+    countEntry = prompt("Enter 1) Add Contact 0) Exit: ");
+    if (countEntry == 1) {
+        let FirstName = prompt("Enter Firstname: ");
+        let LastName = prompt("Enter Lastname: ");
+        let Address = prompt("Enter Address: ");
+        let City = prompt("Enter City name: ");
+        let State = prompt("Enter State name: ");
+        let Zip = prompt("Enter pincode: ");
+        let PhoneNumber = prompt("Enter phone number: ");
+        let EmailId = prompt("Enter email id: ");
+        try {
+            let person = new Contact(FirstName, LastName, Address, City, State, Zip, PhoneNumber, EmailId);
+            addressBookArray.push(person);
+            console.log("Contact is added: ");
+        } catch (e) {
+            console.log(e);
+        }
+    }
+} while (countEntry != 0);
