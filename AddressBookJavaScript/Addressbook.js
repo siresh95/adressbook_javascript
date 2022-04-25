3da10b713e5208ae3ff7042a9455f98081221735
Resh@308 <reshsingh1995@gmail.com>
2022-04-25 11:41:01 +0530

    Added:Feature to create contact in Addressbook

--- AddressBookJavaScript/Addressbook.js
+++ AddressBookJavaScript/Addressbook.js
@@ -1,1 +1,32 @@
-console.log("welcome to address book");
+class Contact{
+    //property
+    firstName;
+    lastName;
+    address;
+    city;
+    state;
+    zip;
+    phoneNumber;
+    email;
+
+    //constructor
+    constructor(firstName,lastName,address,city,state,zip,phoneNumber,email){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phoneNumber = phoneNumber;
+        this.email = email
+    }
+
+    //method
+    toString(){
+        return "Firstname: "+this.firstName+" Lastname: "+this.lastName+" Address: "+this.address+" City: "+this.city+
+        " State: "+this.state+" Zip: "+this.zip+" Phone Number: "+this.phoneNumber+" Email: "+this.email;
+    }
+}
+
+let person = new Contact("Resh","Singh","Abc","Bhopal","MH",462042,9009009010,"ndbbsmnb@gmail.com");
+console.log(person.toString()); 
